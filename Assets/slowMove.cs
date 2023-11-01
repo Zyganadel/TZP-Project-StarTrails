a1eb981309ba1094fa54062807aefd080b32b995
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-01 09:00:26 -0300

    fix bug

--- Assets/slowMove.cs
+++ Assets/slowMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class slowMove : MonoBehaviour
 {
+    public float xMove;
+    public float yMove;
+    public float zMove = -0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,9 @@ public class slowMove : MonoBehaviour
     void Update()
     {
         Vector3 p = transform.position;
-        p.z -= 1 * Time.deltaTime;
+        p.x += xMove * Time.deltaTime;
+        p.y += yMove * Time.deltaTime;
+        p.z += zMove * Time.deltaTime;
         transform.position = p;
     }
 }
