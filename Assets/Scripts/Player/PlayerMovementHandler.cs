2e0adf2ca32f6c4c5ec2833e5618031e0ea6a281
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-08 10:12:40 -0400

    Add roll feature.

--- Assets/Scripts/Player/PlayerMovementHandler.cs
+++ Assets/Scripts/Player/PlayerMovementHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovementHandler : MonoBehaviour
 {
-    public float lookSensitivity = 0.3f;
+    public float lookSensitivity = 1f;
+    public float rollSensitivity = 1;
     public float throttleMax = 10.0f;
     public float throttleMin = 0.0f;
     public float throttleValue = 0.0f;
     float throttleMod;
+    float rollValue;
     public Vector2 lookValue;
     private Transform tf;
     private Rigidbody rb;
@@ -38,20 +41,30 @@ public class PlayerMovementHandler : MonoBehaviour
     public void OnLook(InputValue value)
     {
         lookValue = value.Get<Vector2>();
-        //rotate the parent of player stuff by the look value, but multiply it by 0.5
-        rb.AddRelativeTorque(lookValue.y * lookSensitivity, lookValue.x * lookSensitivity, 0.0f);
+    }
+
+    void OnRoll(InputValue value)
+    {
+        rollValue = value.Get<float>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float t = Time.deltaTime * speedMult;
+        float t = Time.deltaTime;
+        float timedRot = t * lookSensitivity * 50;
+        float timedRoll = t * rollSensitivity * 50;
 
-        throttleValue += throttleMod * Time.deltaTime;
+        //rotate the parent of player stuff by whatever values we have.
+        rb.AddRelativeTorque(lookValue.y * timedRot, lookValue.x * timedRot, rollValue * timedRoll);
+
+        float timedMove = t * speedMult;
+
+        throttleValue += throttleMod * t;
 
         if (throttleValue > throttleMax) { throttleValue = throttleMax; }
         if (throttleValue < throttleMin) { throttleValue = throttleMin; }
 
-        rb.AddForce(tf.forward * throttleValue * t);
+        rb.AddForce(tf.forward * throttleValue * timedMove);
     }
 }
