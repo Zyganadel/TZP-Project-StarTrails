157829e8c0794fb292624c5237726dcbaf17961e
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-20 14:15:22 -0400

    Attempt to make instantiation work

--- Assets/Scripts/Weapons/kineticWeapon.cs
+++ Assets/Scripts/Weapons/kineticWeapon.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class kineticWeapon : MonoBehaviour
 {
+    [SerializeField] private float speed = 1.0f;
+    private bool isClone = false;
+
     // Start is called before the first frame update
-    void Start()
+    void OnAwake()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += transform.forward * speed;
+    }
+
+    void Fire()
+    {
+        GameObject clone = Instantiate(this.gameObject); // used this.gameObject for readability
+        kineticWeapon cloneComponent = clone.GetComponent<kineticWeapon>();
+        cloneComponent.isClone = true;
     }
 }
