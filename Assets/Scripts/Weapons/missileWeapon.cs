f05646dd9d260c006d3be5345a89fd1cfb43105b
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2023-11-22 09:26:45 -0400

    Hopefully, missiles move now.

--- Assets/Scripts/Weapons/missileWeapon.cs
+++ Assets/Scripts/Weapons/missileWeapon.cs
@@ -6,14 +6,22 @@ using UnityEngine;
 public class missileWeapon : MonoBehaviour
 {
     public PlayerController origin;
-    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float lookSpeed = 5f;
     private bool isClone = false;
     [SerializeField] private float timer = 5.0f;
+
+    [SerializeField] private Transform target;
 
     // Start is called before the first frame update
     void OnAwake()
     {
-
+        // very sloppy raycast stuff for missile targeting.
+        Ray ray = new Ray();
+        ray.direction = transform.forward;
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit); // to my knowledge, this raycasts in ray's direction, from ray's origin, returning the first thing it hits.
+        target = hit.transform;
     }
 
     // Update is called once per frame
@@ -25,6 +33,17 @@ public class missileWeapon : MonoBehaviour
             timer -= Time.deltaTime;
             if (timer <= 0) { GameObject.Destroy(gameObject); }
         }
+
+        Vector3 targetDirection = target.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = lookSpeed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
     }
 
     void Fire()
